5531d6e4b371d97b3678c81681a8def5cac420a0
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-16 19:32:57 +0800

    Create simple print function

---
+++ Meeting1.py
@@ -0,0 +1,28 @@
+print("Hello World")
+
+print("""A 'Hello world!' program is generally a computer program that outputs or displays the message Hello World""")
+
+# This is comment
+
+"""
+This is multiline comment
+"""
+
+movie_title = "Captain Marvel"
+release_year = 2019
+rating = 7.9
+is_general_audiences = False
+
+print("The movie is " + movie_title)
+
+top_up_amount = 50
+count = 3
+discount = 2
+
+total_payment = top_up_amount * count
+total_discount = discount * count
+payment_after_discount = total_payment - total_discount
+
+print()
+print("Total payment                    : RM " + str(total_payment))
+print("Total payment (after discount)   : RM " + str(payment_after_discount))
