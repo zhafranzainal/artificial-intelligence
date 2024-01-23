c690e298db3c9fad3a6039c677cea11b4a1c2cbe
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-23 18:52:35 +0800

    Create list.py

---
+++ Meeting 2 - Data Structures/list.py
@@ -0,0 +1,25 @@
+grocery_list = ['Bread', 'Cereal', 'Peanut Butter']
+
+print()
+print(grocery_list)
+
+print()
+print(grocery_list[0])
+print(grocery_list[1])
+print(grocery_list[2])
+print(grocery_list[-1])
+
+grocery_list.append('Sausage')
+grocery_list.append('Croissant')
+grocery_list.remove('Bread')
+
+print()
+print(grocery_list)
+
+grocery_list[1] = 'Strawberry Jam'
+
+print()
+print(grocery_list)
+
+print()
+print(len(grocery_list))
