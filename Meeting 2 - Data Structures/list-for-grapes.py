d997f518ab16099b6f0e72d20b2df61fc1d126e3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-23 19:01:36 +0800

    Create list-for-grapes.py

---
+++ Meeting 2 - Data Structures/list-for-grapes.py
@@ -0,0 +1,16 @@
+numbers = [130, 100, 140, 150, 190, 200, 250, 200, 230, 250]
+
+print()
+print(numbers)
+
+numbers.sort()
+print(numbers)
+
+grade_3 = numbers[:4]
+grade_2 = numbers[4:7]
+grade_1 = numbers[7:]
+
+print()
+print(grade_3)
+print(grade_2)
+print(grade_1)
