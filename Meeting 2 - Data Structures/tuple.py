3307a75a9c3b890f1a454f98ac6bec37e2902876
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-23 19:31:13 +0800

    Create tuple.py

---
+++ Meeting 2 - Data Structures/tuple.py
@@ -0,0 +1,28 @@
+subjects = ("Python", "C++", "JavaScript")
+
+print()
+print(subjects)
+print(len(subjects))
+
+print()
+print(subjects[0])
+print(subjects[1])
+print(subjects[2])
+print(subjects[-1])
+print(subjects[0:2])
+
+print()
+print("Python" in subjects)
+print("Java" in subjects)
+
+subject1, subject2, subject3 = subjects
+
+print()
+print(subject1)
+print(subject2)
+print(subject3)
+
+# subjects[1] = "C"
+
+print()
+print(subjects[1])
