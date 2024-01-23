d3b650ab3c8c53b7d2da0ba2cde7b20a9eab45cc
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-01-23 21:22:48 +0800

    Create random-module.py

---
+++ Meeting 2 - Data Structures/random-module.py
@@ -0,0 +1,10 @@
+import random
+
+friend_list = ['Bryan', 'Cia', 'Peter', 'Drake']
+
+random_name = random.choice(friend_list)
+random_number = random.randint(10, 15)
+
+print()
+print(random_name)
+print(random_number)
