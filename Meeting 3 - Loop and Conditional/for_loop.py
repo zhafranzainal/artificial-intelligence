a6d8c45c782f133e0576c2bec3cba202b940e885
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-06 16:49:05 +0800

    Create for_loop.py

---
+++ Meeting 3 - Loop and Conditional/for_loop.py
@@ -0,0 +1,13 @@
+friends_list = ['Amanda', 'Bruno', 'Camila', 'Drake', 'Elvis', 'Freddie', 'George', 'Harry', 'Iggy', 'Joji']
+
+print()
+for i in friends_list:
+    print(i)
+
+print()
+for i in range(3):
+    print(friends_list[i])
+
+print()
+for i in range(0, 10, 1):
+    print(friends_list[i])
