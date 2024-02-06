78217fab5bb5d9787030071f3d54551e9db3a33b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-06 17:27:14 +0800

    Create conditional_loop.py

---
+++ Meeting 3 - Loop and Conditional/conditional_loop.py
@@ -0,0 +1,8 @@
+while True:
+    answer = input("\nDo you want to loop this argument? \nType yes or no: ")
+
+    if answer.lower() == "yes":
+        print("\nOkay let’s go!")
+    else:
+        print("\nGood bye...")
+        break
