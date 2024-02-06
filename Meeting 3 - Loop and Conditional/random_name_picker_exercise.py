c1fefc88b100034cdfe7912defc6e174eeac6680
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-06 18:54:07 +0800

    Create random_name_picker_exercise.py

---
+++ Meeting 3 - Loop and Conditional/random_name_picker_exercise.py
@@ -0,0 +1,15 @@
+import random
+
+names = []
+
+print("\nAdd names to shuffle! \nPress 'S' to stop\n")
+
+while True:
+    add_name = ('Add name: ')
+
+    if add_name != 's':
+        names.append(add_name)
+    :
+        random_name =
+        print("\nChosen person: " + )
+        break
