2d0cd2d4e0cf450e83b3f09aea7da94d33d76162
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-06 17:05:19 +0800

    Create conditional.py

---
+++ Meeting 3 - Loop and Conditional/conditional.py
@@ -0,0 +1,12 @@
+allowance = 10
+spending = 8
+
+print()
+print(allowance < spending)
+print(allowance > spending)
+print(allowance != spending)
+print(allowance == spending)
+
+print()
+print(allowance == 10) and print(spending == 8)
+print(allowance == 12) or print(spending == 8)
