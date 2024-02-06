ca93ad19b9f580db84f6fc8874cc1d76362e7be7
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-06 17:54:59 +0800

    Update for_loop_challenge.py

--- Meeting 3 - Loop and Conditional/for_loop_challenge.py
+++ Meeting 3 - Loop and Conditional/for_loop_challenge.py
@@ -3,6 +3,9 @@ for i in range(3):
     print("I am learning Python")
 
 print()
+[print("I am learning Python") for i in range(3)]
+
+print()
 for i in range(1, 11):
     multiple_of_5 = i * 5
     print(multiple_of_5)
