12c31c04adcd22ffe29048463bf2429277d263ba
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-06 17:18:28 +0800

    Create conditional_practice.py

---
+++ Meeting 3 - Loop and Conditional/conditional_practice.py
@@ -0,0 +1,13 @@
+students_name = ['Alex', 'Bryan', 'Christ', 'Dave', 'Eva']
+students_score = [95, 80, 75, 90, 65]
+students_grade = []
+
+for score in students_score:
+    if score >= 90:
+        students_grade.append('A')
+    elif score >= 70:
+        students_grade.append('B')
+    else:
+        students_grade.append('C')
+
+print(students_grade)
