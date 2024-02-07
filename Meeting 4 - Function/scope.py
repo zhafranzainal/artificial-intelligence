e1119a6429f1d52a8ea2957984895e3f243da7f4
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-07 20:15:19 +0800

    Create scope.py

---
+++ Meeting 4 - Function/scope.py
@@ -0,0 +1,15 @@
+# Global variable
+total = 50
+
+
+def sum_number():
+    first = 10
+    second = 20
+
+    # Local variable
+    total = first + second
+    print(total)
+
+
+print(total)
+sum_number()
