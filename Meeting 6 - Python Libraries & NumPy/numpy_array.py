a11ca4bd7dabdcf54340b6acc5957ae89dd7ff6d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-02-16 03:58:03 +0800

    Add 2d array

--- Meeting 6 - Python Libraries & NumPy/numpy_array.py
+++ Meeting 6 - Python Libraries & NumPy/numpy_array.py
@@ -5,6 +5,17 @@ a = np.array([1, 2, 3, 4, 5])
 b = np.array([6, 7, 8, 9, 10])
 c = np.array(["test", 11, 11.5])
 
+# 2D array
+array_2d = np.array([
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15]
+])
+
+print()
 print(a)
 print(b)
 print(c)
+
+print()
+print(array_2d)
