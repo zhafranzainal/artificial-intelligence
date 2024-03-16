b18dd77812ddd947d37aa97e5568c9743ace61cd
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-03-16 18:24:05 +0800

    Apply various bitwise operations on image

--- Meeting 7 - OpenCV Image Manipulation/image_masking.py
+++ Meeting 7 - OpenCV Image Manipulation/image_masking.py
@@ -12,11 +12,17 @@ mask = np.zeros(img.shape[:2], dtype="uint8")
 # Draw a white filled circle on the mask
 cv2.circle(mask, (160, 200), 165, 255, -1)
 
-# Apply the mask to the image using bitwise_and operation
-img = cv2.bitwise_and(img, img, mask=mask)
+# Apply various bitwise operations on the image using the mask
+img_and = cv2.bitwise_and(img, img, mask=mask)
+img_or = cv2.bitwise_or(img, img, mask=mask)
+img_not = cv2.bitwise_not(img, img, mask=mask)
+img_xor = cv2.bitwise_xor(img, img, mask=mask)
 
 cv2.imshow("Mask", mask)
-cv2.imshow("Image", img)
+cv2.imshow("Bitwise AND", img_and)
+cv2.imshow("Bitwise OR", img_or)
+cv2.imshow("Bitwise NOT", img_not)
+cv2.imshow("Bitwise XOR", img_xor)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
