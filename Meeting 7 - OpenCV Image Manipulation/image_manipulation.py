2cd50160c7ee0c03432fa7e4c84eb418d21c6e78
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-03-16 17:59:09 +0800

    Open windows multiple times to compare

--- Meeting 7 - OpenCV Image Manipulation/image_manipulation.py
+++ Meeting 7 - OpenCV Image Manipulation/image_manipulation.py
@@ -11,16 +11,19 @@ print(img.shape)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
 # Apply a simple averaging filter to the image (uniform blur)
-img = cv2.blur(img, (10, 10))
+img_blurred = cv2.blur(img, (10, 10))
 
 # Apply a box filter with more control over border handling and normalization
-img = cv2.boxFilter(img, -1, (10, 10))
+img_box_filtered = cv2.boxFilter(img, -1, (10, 10))
 
 # Apply a Gaussian blur to the image
 # Gaussian blur preserves edges better compared to simple averaging filters
-img = cv2.GaussianBlur(img, (9, 9), 1)
+img_gaussian_blurred = cv2.GaussianBlur(img, (9, 9), 1)
 
-cv2.imshow("Image", img)
+cv2.imshow("Resized Image", img)
+cv2.imshow("Blurred Image", img_blurred)
+cv2.imshow("Box Filtered Image", img_box_filtered)
+cv2.imshow("Gaussian Blurred Image", img_gaussian_blurred)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
