87647ceef57be57d43e1a4cfd47e79104dda5bb5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-03-17 12:03:51 +0800

    Create roller_coaster_optimized.py

---
+++ Meeting 8 - Roller Coaster Ride (OOP)/roller_coaster_optimized.py
@@ -0,0 +1,41 @@
+class Person:
+    persons = {}
+
+    # constructor for parent class
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+        Person.persons[name] = self
+
+    def say_hello(self):
+        print(f'\nHello {self.name}, nice to meet you!')
+
+    def ride(self):
+        self.say_hello()
+        if self.age > 10 and self.height >= 100:
+            print(f'Congratulations {self.name}! You may ride the roller coaster.')
+        else:
+            print(f'Sorry {self.name}, you may not ride the roller coaster.')
+
+
+# objects
+james = Person("James", 10, 140)
+rose = Person("Rose", 12, 150)
+dove = Person("Dove", 12, 150)
+diva = Person("Diva", 8, 130)
+
+while True:
+
+    person_name = input("\nEnter name: ")
+    person = Person.persons.get(person_name)
+
+    if person:
+        person.ride()
+    else:
+        print("Sorry, you haven't registered yet!")
+
+    answer = input("\nPress 'y' to continue / 'n' to quit: ")
+
+    if answer.lower() == 'n':
+        break
