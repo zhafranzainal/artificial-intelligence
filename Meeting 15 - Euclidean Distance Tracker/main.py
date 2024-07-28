4ba6294fe6e73e3d3c48b49f9873fa692a2a1c90
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-07-28 20:24:25 +0800

    Implement object tracking

--- Meeting 15 - Euclidean Distance Tracker/main.py
+++ Meeting 15 - Euclidean Distance Tracker/main.py
@@ -1,4 +1,7 @@
 import cv2
+from tracker import EuclideanDistTracker
+
+tracker = EuclideanDistTracker()
 
 cap = cv2.VideoCapture("highway.mp4")
 
@@ -23,6 +26,8 @@ while True:
     # find contours coordinates
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    detections = []
+
     for contour in contours:
 
         area = cv2.contourArea(contour)
@@ -31,13 +36,31 @@ while True:
         if area > 100:
             # cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
             x, y, width, height = cv2.boundingRect(contour)
-            cv2.rectangle(
-                roi,
-                (x, y),
-                (x + width, y + height),
-                (0, 255, 0),
-                2
-            )
+            detections.append([x, y, width, height])
+
+    # print(detections)
+    boxes_ids = tracker.update(detections)
+
+    for boxes_id in boxes_ids:
+        x, y, width, height, object_id = boxes_id
+
+        cv2.putText(
+            roi,
+            str(object_id),
+            (x, y - 15),
+            cv2.FONT_HERSHEY_COMPLEX,
+            1,
+            (255, 0, 0),
+            2
+        )
+
+        cv2.rectangle(
+            roi,
+            (x, y),
+            (x + width, y + height),
+            (0, 255, 0),
+            2
+        )
 
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
---
+++ Meeting 15 - Euclidean Distance Tracker/tracker.py
@@ -0,0 +1,49 @@
+import math
+
+
+class EuclideanDistTracker:
+
+    def __init__(self):
+        self.center_points = {}
+        self.id_count = 0
+
+    def update(self, objects_rect):
+
+        objects_bbs_ids = []
+
+        # find center point coordinates of each detected object
+        for rect in objects_rect:
+
+            x, y, width, height = rect
+            cx = (x + x + width) // 2
+            cy = (y + y + height) // 2
+            # print(cx, cy)
+
+            is_same_object = False
+
+            for object_id, point in self.center_points.items():
+
+                distance = math.hypot(cx - point[0], cy - point[1])
+
+                # if distance lower than 50, assume objects are the same object
+                if distance < 50:
+                    self.center_points[object_id] = (cx, cy)
+                    objects_bbs_ids.append([x, y, width, height, object_id])
+                    is_same_object = True
+                    break
+
+            if not is_same_object:
+                self.center_points[self.id_count] = (cx, cy)
+                objects_bbs_ids.append([x, y, width, height, self.id_count])
+                self.id_count += 1
+
+            new_center_points = {}
+
+            for obj_bb_id in objects_bbs_ids:
+                Nx, Ny, Nw, Nh, object_id = obj_bb_id
+                center = self.center_points[object_id]
+                new_center_points[object_id] = center
+
+            self.center_points = new_center_points.copy()
+
+        return objects_bbs_ids
