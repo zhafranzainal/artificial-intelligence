8e0ed460b0452e9d6179237ba34f0bcb097ce637
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-09-11 05:50:40 +0800

    Detect hand landmarks

---
+++ Meeting 20 - Find Landmark Position/VirtualPainter.py
@@ -0,0 +1,41 @@
+import cv2
+import os
+import HandTrackingModule as Htm
+
+detector = Htm.HandDetector(detection_confidence=0.85)
+
+cap = cv2.VideoCapture(0)
+
+# set canvas width
+cap.set(3, 1280)
+
+# set canvas height
+cap.set(4, 720)
+
+myListDirectory = os.listdir("header")
+print(myListDirectory)
+overlayList = []
+
+for imPath in myListDirectory:
+    image = cv2.imread(f'header/{imPath}')
+    overlayList.append(image)
+
+header = overlayList[0]
+
+while True:
+
+    res, frame = cap.read()
+
+    # flip frames horizontally
+    frame = cv2.flip(frame, 1)
+    frame = detector.find_hands(frame)
+
+    frame[0: 125, 0:1280] = header
+
+    cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
