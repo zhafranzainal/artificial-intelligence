964dc24edede3b6ce8040dace409479b756a0609
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-09-11 22:40:36 +0800

    Swap files

--- Meeting 19 - Hand Landmarks Detection/VirtualPainter.py
+++ Meeting 19 - Hand Landmarks Detection/VirtualPainter.py
@@ -1,6 +1,5 @@
 import cv2
 import os
-import numpy as np
 import HandTrackingModule as Htm
 
 detector = Htm.HandDetector(detection_confidence=0.85)
@@ -23,15 +22,6 @@ for imPath in myListDirectory:
 
 header = overlayList[0]
 
-# default brush color is red
-drawColor = (0, 0, 255)
-brushThickness = 7
-eraserThickness = 40
-
-# brush start coordinates
-xp, yp = 0, 0
-imgCanvas = np.zeros((720, 1280, 3), np.uint8)
-
 while True:
 
     res, frame = cap.read()
@@ -39,74 +29,10 @@ while True:
     # flip frames horizontally
     frame = cv2.flip(frame, 1)
     frame = detector.find_hands(frame)
-    landmarkList = detector.find_position(frame, draw=True)
-    print(landmarkList)
-
-    if len(landmarkList) != 0:
-        x1, y1 = landmarkList[8][1:]
-        x2, y2 = landmarkList[12][1:]
-        print(x1, y1, x2, y2)
-
-        fingers = detector.fingers_up()
-        print(fingers)
-
-        # if index and middle finger are lifted
-        if fingers[1] and fingers[2]:
-
-            xp, yp = 0, 0
-            print("Selection mode")
-
-            # draw rectangle indicating selection mode
-            cv2.rectangle(frame, (x1, y1 - 25), (x2, y2 + 25), drawColor, cv2.FILLED)
-
-            if y1 < 125:
-                if 320 < x1 < 480:
-                    header = overlayList[0]
-                    drawColor = (0, 0, 255)
-                elif 480 < x1 < 630:
-                    header = overlayList[1]
-                    drawColor = (0, 255, 0)
-                elif 630 < x1 < 840:
-                    header = overlayList[2]
-                    drawColor = (255, 0, 0)
-                elif x1 > 1000:
-                    header = overlayList[3]
-                    # black color as the eraser mode
-                    drawColor = (0, 0, 0)
-
-        if not (fingers[1] and fingers[2]):
-
-            print("Drawing Mode")
-
-            # draw circle indicating drawing mode
-            cv2.circle(frame, (x1, y1), 15, drawColor, cv2.FILLED)
-
-            # if brush coordinate still at initial coordinate, overwrite with tip index finger nodes landmark location
-            if xp == 0 and yp == 0:
-                xp, yp = x1, y1
-
-            # if choose eraser mode
-            if drawColor == (0, 0, 0):
-                cv2.line(frame, (xp, yp), (x1, y1), drawColor, eraserThickness)
-                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, eraserThickness)
-            # else choose brush colour
-            else:
-                cv2.line(frame, (xp, yp), (x1, y1), drawColor, brushThickness)
-                cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThickness)
-
-            xp, yp = x1, y1
-
-    # persist drawn image in video frame
-    frameGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
-    _, frameInverse = cv2.threshold(frameGray, 50, 255, cv2.THRESH_BINARY_INV)
-    frameInverse = cv2.cvtColor(frameInverse, cv2.COLOR_GRAY2BGR)
-    frame = cv2.bitwise_and(frame, frameInverse)
-    frame = cv2.bitwise_or(frame, imgCanvas)
 
     frame[0: 125, 0:1280] = header
 
     cv2.imshow("Frame", frame)
-    cv2.imshow("Canvas", imgCanvas)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
--- Meeting 20 - Find Landmark Position/HandTrackingModule.py
+++ Meeting 20 - Find Landmark Position/HandTrackingModule.py
@@ -16,6 +16,7 @@ class HandDetector:
         )
         self.mpDraw = mp.solutions.drawing_utils
         self.results = None
+        self.landmarkList = None
 
     def find_hands(self, frame, draw=True):
 
@@ -30,3 +31,44 @@ class HandDetector:
                     self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS)
 
         return frame
+
+    def find_position(self, frame, hand_no=0, draw=True):
+
+        self.landmarkList = []
+
+        if self.results.multi_hand_landmarks:
+
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+
+            for index, landmark in enumerate(my_hand.landmark):
+
+                h, w, c = frame.shape
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                self.landmarkList.append([index, cx, cy])
+
+                if draw:
+                    cv2.circle(frame, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
+
+        return self.landmarkList
+
+    def fingers_up(self):
+
+        fingers = []
+
+        # list for the ID nodes of fingertip landmarks
+        tip_ids = [4, 8, 12, 16, 20]
+
+        # condition to check if thumb is up
+        if self.landmarkList[tip_ids[0]][1] < self.landmarkList[tip_ids[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # index for the fingers from index finger to pinky finger
+        for finger_id in range(1, 5):
+            if self.landmarkList[tip_ids[finger_id]][2] < self.landmarkList[tip_ids[finger_id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        return fingers
