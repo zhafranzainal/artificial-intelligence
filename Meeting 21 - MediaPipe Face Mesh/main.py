e2f892d84c5a62d4bc3146f815d1e12a538f617f
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-10-06 14:29:45 +0800

    Draw landmarks using tesselation and contours style

--- Meeting 21 - MediaPipe Face Mesh/main.py
+++ Meeting 21 - MediaPipe Face Mesh/main.py
@@ -1,4 +1,5 @@
 import cv2
+import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
 
@@ -8,9 +9,30 @@ cap.set(3, 640)
 # set canvas height
 cap.set(4, 480)
 
+mpDraw = mp.solutions.drawing_utils
+mpDrawingStyles = mp.solutions.drawing_styles
+
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=4)
+
 while True:
 
     ret, frame = cap.read()
+
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(rgb)
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(image=frame, landmark_list=face_landmarks,
+                                  connections=mpFaceMesh.FACEMESH_TESSELATION, landmark_drawing_spec=None,
+                                  connection_drawing_spec=mpDrawingStyles.get_default_face_mesh_tesselation_style()
+                                  )
+            mpDraw.draw_landmarks(image=frame, landmark_list=face_landmarks,
+                                  connections=mpFaceMesh.FACEMESH_CONTOURS, landmark_drawing_spec=None,
+                                  connection_drawing_spec=mpDrawingStyles.get_default_face_mesh_contours_style()
+                                  )
+
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1) & 0xFF
